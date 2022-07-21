e4341454c679ee9fd252c33b77140c3de5583a24
ChandraMouli <73635999+vermilion000@users.noreply.github.com>
2022-07-21 08:13:53 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,12 @@
+
+
+a = {99,138,57,6,2,3}      ###set variables can be changed according to wish
+b = {1,4,6,9,72}
+
+print("A = " +str(a))       ###statements to display sets 
+print("B = " + str(b))
+
+print("Union of A and B is :" + str(a|b) )
+print("Intersection of A and B is :" + str(a&b) )       ##operations specified to display |,&,-,^
+print("Difference of A and B is :" + str(a-b) )
+print("Symmetric Difference of A and B is :" + str(a^b) )
